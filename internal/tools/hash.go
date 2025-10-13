49fe12b67b08459cc6d4b30385a46cee2e93a91f
Richard-Persson <ricpersson00@gmail.com>
2025-10-13 09:07:27 +0200

    minor adjustments

--- internal/tools/hash.go
+++ internal/tools/hash.go
@@ -3,17 +3,16 @@ import (
   "golang.org/x/crypto/bcrypt"
 )
 
-// HashPassword takes a plaintext password and returns the bcrypt hash.
+// Takes a plaintext password and returns the bcrypt hash.
 func HashPassword(password string) (string, error) {
   hashBytes, err := bcrypt.GenerateFromPassword(
     []byte(password),
-    bcrypt.DefaultCost, // ~12 by default
+    bcrypt.DefaultCost, 
   )
   return string(hashBytes), err
 }
 
-// CheckPassword compares a bcrypt hash with its possible plaintext equivalent.
-// Returns true on match, false otherwise.
+// Compares a bcrypt hash with its possible plaintext equivalent.
 func CheckPassword(hash, password string) bool {
   err := bcrypt.CompareHashAndPassword(
     []byte(hash),
