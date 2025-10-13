5bea3e244e68cf4e651a07722414ceaed89b07ca
Richard-Persson <ricpersson00@gmail.com>
2025-10-13 14:17:24 +0200

    added seconds to formatter

--- internal/tools/date-time-hours.go
+++ internal/tools/date-time-hours.go
@@ -1,16 +1,20 @@
 package tools
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 
 
 func DateTimeHoursFormatter(date string, start string, end string) (time.Time, time.Time, time.Time, float64, error)  {
 
+	fmt.Printf("\ndate: %v | start: %v | end: %v\n",date,start,end)
 
 	var total_hours float64 = 0
 	newDate, dateParseErr := time.Parse("2006-01-02",date)
-	startTime, timeParseErr1 := time.Parse("15:04",start)
-	endTime, timeParseErr2 := time.Parse("15:04",end)
+	startTime, timeParseErr1 := time.Parse("15:04:05",start)
+	endTime, timeParseErr2 := time.Parse("15:04:05",end)
 
 	if dateParseErr != nil {
 		return newDate, startTime, endTime, total_hours, dateParseErr
